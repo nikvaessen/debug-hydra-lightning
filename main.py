984ee43788711bca891e9d01f9f3a13889f18fd9
nvaessen <nvaessen@science.ru.nl>
2023-06-01 10:54:40 +0200

    try datapipes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import functools
 import time
 
 import lightning
 import hydra
 import torch
 
-from torch.utils.data import Dataset, DataLoader
+from torchdata.datapipes.iter import *
+
+from torch.utils.data import DataLoader
 
 
 class Network(torch.nn.Module):
@@ -17,12 +20,23 @@ class Network(torch.nn.Module):
         return self.fc1(x)
 
 
-class Data(Dataset):
-    def __len__(self):
-        return 10_000
+def get_sample(seed: int):
+    rng = torch.random.manual_seed(seed)
 
-    def __getitem__(self, item):
-        return torch.rand((1000,))
+    return torch.rand((1000,), generator=rng)
+
+
+def construct_datapipe(shard_workers: bool = False):
+    dp = IterableWrapper(range(0, 10_000))
+    dp = Cycler(dp)
+    dp = Shuffler(dp)
+
+    if shard_workers:
+        dp = ShardingFilter(dp)
+
+    dp = Mapper(dp, get_sample)
+
+    return dp
 
 
 @hydra.main(config_path="config", config_name="main", version_base="1.3")
@@ -35,18 +49,19 @@ def main(cfg):
     network = Network()
     network, opt = fabric.setup(network, torch.optim.Adam(network.parameters()))
 
-    train_dataloader = fabric.setup_dataloaders(DataLoader(Data(), num_workers=4))
+    train_dataloader = fabric.setup_dataloaders(
+        DataLoader(construct_datapipe(cfg.shard_workers), num_workers=cfg.num_workers)
+    )
 
-    # keep alive
-    while True:
-        for x in train_dataloader:
-            y = network(x)
-            y_target = torch.randint(0, 9, size=(1,), device=fabric.device)
-            loss = torch.nn.functional.cross_entropy(y, y_target)
-            print(loss)
+    # dataloader is infinite
+    for x in train_dataloader:
+        y = network(x)
+        y_target = torch.randint(0, 9, size=(1,), device=fabric.device)
+        loss = torch.nn.functional.cross_entropy(y, y_target)
+        print(loss)
 
-            fabric.backward(loss)
-            opt.step()
+        fabric.backward(loss)
+        opt.step()
 
 
 if __name__ == "__main__":
