c7031897edb5dc31e3831c494c17b9f5d6931900
nvaessen <nvaessen@science.ru.nl>
2023-06-01 10:25:08 +0200

    more mem

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import torch
 
 @hydra.main(config_path="config", config_name="main", version_base="1.3")
 def main(cfg):
-    fabric = lightning.Fabric(devices=cfg.devices, num_nodes=cfg.num_nodes)
+    fabric = lightning.Fabric(
+        accelerator="gpu", devices=cfg.devices, num_nodes=cfg.num_nodes
+    )
     fabric.launch()
 
     # use gpu so process shows up in nvidia-smi
-    tensor = torch.tensor([0]).to(fabric.device)
+    tensor = torch.arange(0, 100_000, device=fabric.device)
 
     # keep alive
     while True:
